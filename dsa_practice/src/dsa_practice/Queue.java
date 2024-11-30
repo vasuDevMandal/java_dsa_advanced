9abfb3fdfa9a61a17ac3d5f98a941b7dce9756fa
Vasudev-desktop <code.vasu@gmail.com>
2024-11-30 18:49:09 +0530

    Queue - update 1

---
+++ dsa_practice/src/dsa_practice/Queue.java
@@ -0,0 +1,85 @@
+package dsa_practice;
+
+import java.util.NoSuchElementException;
+
+public class Queue {
+
+	private ListNode front;
+	private ListNode rear;
+	private int length;
+	
+	private class ListNode{
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data){
+			this.data = data;
+			this.next = null;
+		}
+	}
+	public int length() {
+		return length;
+	}
+	public boolean isEmpty() {
+		return length == 0;		
+	}
+	public void Enqueue(int data) {
+		ListNode newNode = new ListNode(data);
+		if(isEmpty()) {
+			front = newNode;
+		}else {
+			rear.next = newNode;
+		}
+		rear = newNode;
+		System.out.println("Enqueue: " + rear.data);
+		length++;
+	}
+	public ListNode Dequeue() {
+		if(isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		ListNode temp = front;
+		front = front.next;
+		temp.next = null;
+		if(front==null) {
+			rear = null;
+		}
+		length--;
+		System.out.println("Dequeue: " + temp.data);
+		return temp;
+		
+	}
+	public void print() {
+//		if(isEmpty()) {
+//			return;
+//		}
+		ListNode temp = front;
+//		System.out.print("*QUEUE* | length: " + length() +"\nfront => ");
+		while(temp!=null) {
+			System.out.print( temp.data + " -> ");
+			temp = temp.next;
+		}
+//		System.out.print("null <= rear\n" );
+		System.out.print("null\n" );
+	}
+	
+	public static void main(String[] args) {
+		Queue q = new Queue();
+		
+		q.Enqueue(1);q.print();
+		q.Enqueue(2);q.print();
+		q.Enqueue(3);q.print();
+		q.Enqueue(4);q.print();
+		
+		q.Dequeue();q.print();
+		q.Dequeue();q.print();
+		q.Dequeue();q.print();
+		q.Dequeue();q.print();
+		
+		System.out.println("frontNode: " + q.front);
+		System.out.println("rearNode: " + q.rear);
+		
+		q.Dequeue();q.print();
+	}
+	
+}
--- dsa_practice/src/dsa_practice/StackArr.java
+++ dsa_practice/src/dsa_practice/StackArr.java
@@ -1,5 +1,54 @@
 package dsa_practice;
 
-public class StackArr {
+import java.util.Scanner;
 
+class Stack2{
+	int n = 10;
+    int top = -1;
+    int[] a = new int[n];  
+    void push(int val){
+        if(top == (n-1)){
+            System.out.println("overflow");
+        }else{
+            top = top + 1;
+            a[top] = val;
+            System.out.println("inserted: " + a[top]);
+        }
+    }
+
+    void pop(){
+        if(top == -1) {
+        	System.out.println("underflow");
+        }else {
+        	top = top -1;
+        	System.out.println("deleted: " + a[top]);
+        }
+    }
+    
+    void display() {
+    	System.out.print("\ntop");
+    	for(int i = 0; i <= top; i++) {
+    		System.out.print( "->" + a[i] );
+    	}
+    }
+}
+
+
+public class StackArr {
+	
+	public static void main(String[] args) {
+		
+		Stack2 stack = new Stack2();
+		stack.push(10);stack.display();
+		stack.push(20);stack.display();
+		stack.push(30);stack.display();
+		
+		stack.pop();
+		
+		stack.display();
+
+			
+		
+		
+	}
 }
