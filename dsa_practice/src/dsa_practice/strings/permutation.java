2b9866c34236165c83717cc026f145e92d99877f
Vasudev-desktop <code.vasu@gmail.com>
2024-12-27 00:14:53 +0530

    permutation in strings via recursion - update 2

--- dsa_practice/src/dsa_practice/strings/permutation.java
+++ dsa_practice/src/dsa_practice/strings/permutation.java
@@ -1,9 +1,13 @@
 package dsa_practice.strings;
 
+import java.util.ArrayList;
+
 public class permutation {
     public static void main(String[] args) {
 
         permutations("","abc");
+
+        System.out.println(permutationsAL("","def"));
     }
 
     static void permutations(String p , String up){
@@ -20,5 +24,27 @@ public class permutation {
 
             permutations(f + ch + s, up.substring(1) );
         }
+    }
+
+    static ArrayList<String> permutationsAL(String p , String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+         }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> answer = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+
+            String f = p.substring(0,i);
+            String s = p.substring(i, p.length());
+
+            answer.addAll(permutationsAL(f + ch + s, up.substring(1) ));
+        }
+
+        return answer;
     }
 }
