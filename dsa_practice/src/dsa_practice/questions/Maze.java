86e77bc95c10876853ab3e66e5032d01f25cde60
Vasudev-desktop <code.vasu@gmail.com>
2024-12-28 23:48:58 +0530

    recursion practice - maze -update 1

---
+++ dsa_practice/src/dsa_practice/questions/Maze.java
@@ -0,0 +1,17 @@
+package dsa_practice.questions;
+
+public class Maze {
+    public static void main(String[] args) {
+        System.out.println(count(3,3));
+    }
+
+    static int count(int r, int c){
+        if(r==1 || c==1){
+            return 1;
+        }
+        int right = count(r,c-1 );
+        int left = count(r-1, c);
+
+        return right + left;
+    }
+}
