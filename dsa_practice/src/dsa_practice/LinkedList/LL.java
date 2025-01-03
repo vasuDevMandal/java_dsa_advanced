7e5d83fb20936771ef2e3a46af63099346fe21ed
Vasudev-desktop <code.vasu@gmail.com>
2025-01-03 20:50:34 +0530

    Linked-list - update 2

--- dsa_practice/src/dsa_practice/LinkedList/LL.java
+++ dsa_practice/src/dsa_practice/LinkedList/LL.java
@@ -1,4 +1,23 @@
 package dsa_practice.LinkedList;
 
 public class LL {
+
+    private Node head;
+    private Node tail;
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int val) {
+            this.value = val;
+        }
+        public Node(int val, Node next){
+            this.value = val;
+            this.next = next;
+        }
+
+    }
+
+
 }
