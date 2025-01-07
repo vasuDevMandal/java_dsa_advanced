57ed31421cfe027c060dd7ac37194cddefccb3a0
Vasudev-desktop <code.vasu@gmail.com>
2025-01-07 14:12:40 +0530

    Linked-list: question - qus: find cycle length and starting point

--- dsa_practice/src/dsa_practice/CircularLinkedList.java
+++ dsa_practice/src/dsa_practice/CircularLinkedList.java
@@ -3,7 +3,8 @@ package dsa_practice;
 import java.util.NoSuchElementException;
 
 public class CircularLinkedList {
-	
+
+	private ListNode head;//extra pointer added check git history
 	private ListNode last;
 	private int length;
 
@@ -84,38 +85,148 @@ public class CircularLinkedList {
 		return first;
 	}
 
+	public boolean findCycle(ListNode node){
+		ListNode fast = node;
+		ListNode slow = node;
+
+		while(fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+			if(fast == slow){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int findCycleLength(ListNode node){
+		ListNode fast = node;//head
+		ListNode slow = node;//head
+
+		while(fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+			if(fast == slow){
+				int length = 0;
+				ListNode temp = slow;
+				do{
+					temp = temp.next;
+					length++;
+				}while(temp != slow);
+
+				return length;
+			}
+		}
+		return -1;
+	}
+
+	//floyed warshall cycle detection
+	public ListNode findCycleStartNode(ListNode node){
+		ListNode fast = node;
+		ListNode slow = node;
+
+		while(fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+			if(fast == slow){
+//				return floyedCycleDetect(slow);
+				ListNode temp = head;
+				while(slow != temp){
+					temp = temp.next;
+					slow = slow.next;
+				}
+				return temp;
+			}
+		}
+		return null;
+	}
+
+	private ListNode floyedCycleDetect(ListNode slowPtr){
+		ListNode temp = head;
+		while(slowPtr != temp){
+			temp = temp.next;
+			slowPtr = slowPtr.next;
+		}
+		return temp;
+	}
+
+	//cycle start point second version
+	//https://youtu.be/70tx7KcMROc?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ&t=4429
+	public ListNode findCycleStartNode2(ListNode node){
+		int length = 0;
+		ListNode fast = node;
+		ListNode slow = node;
+
+		while(fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+			if(fast == slow){
+				length = findCycleLength(slow);
+				break;
+			}
+		}
+		//find the start node
+		ListNode first = head;
+		ListNode second = head;
+
+		while(length > 0){
+			second = second.next;
+			length--;
+		}
+
+		//keep moving both forward the both meet at cycle start point
+		while (first != second){
+			first = first.next;
+			second = second.next;
+		}
+
+		return first;
+	}
+
 	public static void main(String[] args) {
 		
 		CircularLinkedList cll = new CircularLinkedList();
-//		ListNode first = new ListNode(1);
-//		ListNode second = new ListNode(5);
-//		ListNode third = new ListNode(10);
-//		ListNode forth = new ListNode(15);
-//		
-//		first.next = second;
-//		second.next = third;
-//		third.next = forth;
-//		forth.next = first;
-//		
-//		cll.last = forth;
+		ListNode first = new ListNode(1);
+		ListNode second = new ListNode(5);
+		ListNode third = new ListNode(10);
+		ListNode forth = new ListNode(15);
+		ListNode fifth = new ListNode(22);
+		ListNode sixth = new ListNode(30);
+
+		first.next = second;
+		second.next = third;
+		third.next = forth;
+		forth.next = fifth;
+		fifth.next = sixth;
+		sixth.next = third;
+
+		cll.last = sixth;
+		cll.head = first;
 		
 		//inserting manually
-		cll.insertFirst(5);
-		cll.insertFirst(10);
-		cll.insertFirst(15);
-		cll.insertFirst(20);
+//		cll.insertFirst(5);
+//		cll.insertFirst(10);
+//		cll.insertFirst(15);
+//		cll.insertFirst(20);
+//
+//		cll.insertLast(40);
+//		cll.insertLast(45);
 		
-		cll.insertLast(40);
-		cll.insertLast(45);
-		
+//		cll.printList();
+//		System.out.println("deleted first: " + cll.removeFirst().data);
+//		System.out.println("deleted first: " + cll.removeFirst().data);
+//		System.out.println("deleted first: " + cll.removeFirst().data);
+//		System.out.println("deleted first: " + cll.removeFirst().data);
+//		System.out.println("deleted first: " + cll.removeFirst().data);
+//		System.out.println("deleted first: " + cll.removeFirst().data);
 		cll.printList();
-		System.out.println("deleted first: " + cll.removeFirst().data); 
-		System.out.println("deleted first: " + cll.removeFirst().data); 
-		System.out.println("deleted first: " + cll.removeFirst().data); 
-		System.out.println("deleted first: " + cll.removeFirst().data); 
-		System.out.println("deleted first: " + cll.removeFirst().data); 
-		System.out.println("deleted first: " + cll.removeFirst().data); 
-		cll.printList();
-		
+
+		System.out.println("cycle exist: " + cll.findCycle(cll.head));
+
+		System.out.println("cycle length: " + cll.findCycleLength(cll.last.next));
+
+		System.out.println("cycle start point: " + cll.findCycleStartNode(cll.head).data);
+		System.out.println("cycle start point - 2 : " + cll.findCycleStartNode2(cll.head).data);
+
 	}
 }
