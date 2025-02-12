9bd67e3ba6389571e8796162f349f8e2ec5a37a5
Vasudev-desktop <code.vasu@gmail.com>
2025-02-12 10:59:22 +0530

    questions-valid parenthesis | leetcode-20

---
+++ dsa_practice/src/leetcodeProblems/ValidParenthesis.java
@@ -0,0 +1,47 @@
+package leetcodeProblems;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+
+    boolean checkParenthesis(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else {
+                if(ch == ')'){
+                    if(stack.isEmpty() || stack.pop() != '(' ){
+                        return false;
+                    }
+                }
+                if(ch == ']'){
+                    if(stack.isEmpty() || stack.pop() != '['){
+                        return false;
+                    }
+                }
+                if(ch == '}'){
+                    if(stack.isEmpty() || stack.pop() != '{'){
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+
+    public static void main(String[] args) {
+        ValidParenthesis cp = new ValidParenthesis();
+        String str1 = "[[{(())}]]";//correct
+        String str2 = "[[(()}]]";//wrong
+
+        System.out.println(str1 +" | isValid: " + cp.checkParenthesis(str1));
+        System.out.println(str2 +" | isValid: " + cp.checkParenthesis(str2));
+    }
+
+}
+
+
