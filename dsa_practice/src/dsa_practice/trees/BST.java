c0058e87fa828fb6b874a8a9f0bdf1c133818bac
Vasudev-desktop <code.vasu@gmail.com>
2025-02-22 02:33:40 +0530

    find cousins - binary tree

--- dsa_practice/src/dsa_practice/trees/BST.java
+++ dsa_practice/src/dsa_practice/trees/BST.java
@@ -218,6 +218,56 @@ class BST {
         return result;
     }
 
+    //https://leetcode.com/problems/cousins-in-binary-tree/
+    public boolean isCousins(Node root, int x, int y) {
+        Node xx = findNode(root, x);
+        Node yy = findNode(root, y);
+
+        return ( nodeLevel(root, xx, 0) == nodeLevel(root, yy, 0)) && (!isSibling(root, xx, yy));
+    }
+
+    private Node findNode(Node node, int x){
+        if(node == null){
+            return null;
+        }
+        if(node.value == x){
+            return node;
+        }
+        Node n = findNode(node.left,x);
+        if(n!=null){
+            return n;
+        }
+        return findNode(node.right, x);
+    }
+
+    boolean isSibling(Node node, Node x, Node y){
+        if(node == null){
+            return false;
+        }
+        return (
+                (node.left == x && node.right == y) || (node.left == y && node.right == x)
+                        || isSibling(node.left, x, y) || isSibling(node.right, x, y)
+                );
+    }
+
+    int nodeLevel(Node node, Node x, int level){
+        if(node == null){
+            return 0;
+        }
+        if (node == x){
+            return level;
+        }
+
+        int currentLevel = nodeLevel(node.left, x, level+1);
+        if ( currentLevel != 0 ){
+            return currentLevel;
+        }
+        return nodeLevel(node.right, x, level+1);
+    }
+
+
+
+
     //google asked
     public Node findLevelOrderSuccessor(Node root, int key){
         if(root == null){
