04f33edce3a8464ccab9dc190b9a71229a6146b5
vasudev <code.vasu@gmail.com>
2025-07-31 17:00:14 +0530

    array questions

--- dsa_practice/src/arraysQus/arrayQusEsy.java
+++ dsa_practice/src/arraysQus/arrayQusEsy.java
@@ -1,10 +1,60 @@
 package arraysQus;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class arrayQusEsy {
     public static void main(String[] args) {
 
+//        int[][] mat = {{1,2},{2,3},{3,4}};
+//        System.out.println(mat.length);
+//        System.out.println(mat[0].length);
+
+        System.out.println(addToArrayForm(new int[] {1,2,0,0},34));
+//        System.out.println(32454%10);
+//        System.out.println(32454/10);
+    }
+
+
+    //989. Add to Array-Form of Integer
+    static List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> result = new ArrayList<>();
+        int i = num.length-1;
+
+        while (i>=0 || k>0){//first k will finish
+            if(i>=0){
+                k += num[i];
+                i--;
+            }
+            result.add(k%10);//add the last digit
+            k/=10;//drop the last digit
+
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    //1252. Cells with Odd Values in a Matrix
+    public int oddCells(int m, int n, int[][] indices) {
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        int counter = 0;
+
+        for(int[] k : indices){
+            row[k[0]]++;
+            col[k[1]]++;
+        }
+        for(int i = 0; i < row.length; i++){
+            for(int j = 0; j < col.length; j++){
+                if( (row[i] + col[j])%2 != 0 ) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 
     // 832. Flipping an Image
