186ba07a73f8d577f8e3c9cb6850f40f1c0160f4
vasudev <code.vasu@gmail.com>
2025-08-05 00:57:49 +0530

    recursion qus 1

---
+++ dsa_practice/src/RecursionQus/RecursionQuestions.java
@@ -0,0 +1,4 @@
+package RecursionQus;
+
+public class RecursionQuestions {
+}
--- dsa_practice/src/StringsQus/StringQuestions.java
+++ dsa_practice/src/StringsQus/StringQuestions.java
@@ -4,9 +4,41 @@ import java.util.Arrays;
 
 public class StringQuestions {
     public static void main(String[] args) {
-        String address = "1.1.1.1";
-        System.out.println(defangIPaddr(address));
-        System.out.println(restoreString("codeleet", new int[] {4,5,6,7,0,2,1,3}));
+//        String address = "1.1.1.1";
+//        System.out.println(defangIPaddr(address));
+//        System.out.println(restoreString("codeleet", new int[] {4,5,6,7,0,2,1,3}));
+        System.out.println(sortSentence("is2 sentence4 This1 a3"));
+    }
+    //1859. Sorting the Sentence
+    static String sortSentence(String s) {
+        String[] wordArr = s.split(" ");
+        String[] newArr = new String[wordArr.length];
+        for (int i = 0; i < wordArr.length; i++) {
+            System.out.print(wordArr[i]);
+            int index =  wordArr[i].charAt(wordArr[i].length()-1) - '1' ;
+            System.out.print(" | index: "+ index);
+            String str = wordArr[i].substring(0,wordArr[i].length()-1);
+            System.out.println( " | word: " + str);
+            newArr[index] = str;
+        }
+        return String.join(" ", newArr);
+    }
+    //1678. Goal Parser Interpretation
+    static String interpret(String command) {
+//        return command.replace("()", "o").replace("(al)","al");
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < command.length(); i++){
+            if(command.charAt(i) == 'G'){
+                sb.append('G');
+            }else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+                sb.append('o');
+                i++;
+            }else if(command.charAt(i) == '(' && command.charAt(i+1) == 'a'){
+                sb.append("al");
+                i+=3;
+            }
+        }
+        return sb.toString();
     }
 
     //1528. Shuffle String
